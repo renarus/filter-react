38bf1df49d7e843e02f1dfbd62b38b17ae242de8
DESKTOP-BVVFQ66\ACER <renarus>
2023-10-19 00:34:27 +0400

    adding-style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState } from 'react'
+import data from './data'
+const App = () => {
+  const [search,setSearch]=useState('')
+  console.log(search)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-center text-danger'>List of Users</h1>
+      <form className='d-flex justify-content-center align-items-center'>
+        <input type="search"  
+        className='form-control  ml-3' placeholder='Search user'
+          onChange={(e)=>{setSearch(e.target.value)}}
+      />
+      </form>
+      <table className='table table-dark table-bordered lh-3'>
+      <thead className='fs-4'>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Email</th>
+          <th>Gender</th>
+          <th>Ip adress</th>
+          </tr>
+        </thead>
+      {data.filter((item)=>{
+        return search.toLowerCase()==='' ? item : item.first_name.toLowerCase().includes(search)
+      }).map((item)=>(
+        <tbody>
+        <tr key={item.id} className='table-light lh-lg'>
+          <td>{item.first_name}</td>
+          <td>{item.last_name}</td>
+          <td>{item.email}</td>
+          <td>{item.gender}</td>
+          <td>{item.ip_address}</td>
+        </tr>
+        </tbody>
+      ))}
+       </table>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
